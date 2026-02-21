f17d450fe85be1e9bdc7ea9f58eac841c71c3b36
pondicherryprep <ramyaravichandran4121999@gmail.com>
2026-02-21 22:43:36 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,30 @@
+const questionBank = {
+  maths: {
+    class6: {
+      algebra: [
+        {
+          question: "What is x if x + 5 = 10?",
+          options: ["2", "5", "10", "15"],
+          answer: 1
+        },
+        {
+          question: "Solve: 3x = 12",
+          options: ["2", "3", "4", "6"],
+          answer: 2
+        }
+      ]
+    }
+  },
+
+  english: {
+    class6: {
+      grammar: [
+        {
+          question: "Choose the correct verb: She ____ going.",
+          options: ["is", "are", "am", "be"],
+          answer: 0
+        }
+      ]
+    }
+  }
+};
